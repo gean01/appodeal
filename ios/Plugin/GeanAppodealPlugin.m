db492912f294c2622c35df808adbce110312cb21
geansantos <gean01@gmail.com>
2023-01-10 02:55:31 -0300

    Add new interface to GeanAppodealPlugin

--- ios/Plugin/GeanAppodealPlugin.m
+++ ios/Plugin/GeanAppodealPlugin.m
@@ -4,6 +4,7 @@
 // Define the plugin using the CAP_PLUGIN Macro, and
 // each method the plugin supports using the CAP_PLUGIN_METHOD macro.
 CAP_PLUGIN(GeanAppodealPlugin, "GeanAppodeal",
-    CAP_PLUGIN_METHOD(showInterstitial, CAPPluginReturnPromise);
     CAP_PLUGIN_METHOD(initializeAppodeal, CAPPluginReturnPromise);
+    CAP_PLUGIN_METHOD(showInterstitial, CAPPluginReturnPromise);
+    CAP_PLUGIN_METHOD(interstitialIsLoaded, CAPPluginReturnPromise);
 )
